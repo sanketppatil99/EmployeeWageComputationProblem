fbf5f06f8575b7e117e22b3fb6e8ad3022871138
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2022-12-03 16:33:35 +0530

    To Check employee DailyWage

---
+++ EmployeeWageProblem/EmployeeWageProblem/DailyWage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+{
+    public static class DailyWage
+    {
+        public static void dailyWage()
+        {
+            int Is_Full_time = 1;
+
+            int Emp_Rate_per_hour = 20;
+           
+            int empHrs = 0;
+            int empWage = 0;
+            Random rand = new Random();
+            int empCheck = rand.Next(2);
+           
+            if (empCheck == Is_Full_time)
+            {
+                Console.WriteLine("Employee is Full Time");
+                empHrs = 8;
+            }
+            else
+            {
+                Console.WriteLine("Employee is not Worked");
+                empHrs = 0;
+            }
+            empWage = Emp_Rate_per_hour * empHrs;
+            Console.WriteLine("Employee Wage =  " + empWage);
+
+        }
+    }
+}
