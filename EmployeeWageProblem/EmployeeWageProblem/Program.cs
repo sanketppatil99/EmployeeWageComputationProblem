f1624e09437a95fc68163e2a204238f3d9c2522f
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2022-12-06 15:57:09 +0530

    Refactor Code

--- EmployeeWageProblem/EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/EmployeeWageProblem/Program.cs
@@ -5,7 +5,7 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program ");
-            TotalWage.totalWage();
+            Refactor.refactor();
         }
     }
 }
---
+++ EmployeeWageProblem/EmployeeWageProblem/Refactor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+{
+    public class Refactor
+    {
+        public static void refactor()
+        {
+            const int Is_Full_time = 1;
+            const int Is_Part_time = 2;
+            const int Emp_Rate_per_hour = 20;
+            const int Num_Of_Working_days = 20;
+            const int toatal_Wroking_Hrs = 100;
+            
+            int totalWorkingDays = 0;
+            int totalHrs = 0;
+            int empHrs = 0;
+            int empWage = 0;
+            while (totalHrs < toatal_Wroking_Hrs && totalWorkingDays < Num_Of_Working_days)
+            {
+                totalWorkingDays++;
+                Random rand = new Random();
+                int empCheck = rand.Next(3);
+
+               
+                switch (empCheck)
+                {
+                    case Is_Full_time:
+                        empHrs = 8;
+                        break;
+
+                    case Is_Part_time:
+                        empHrs = 4;
+                        break;
+
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalHrs += empHrs;
+            }
+            Console.WriteLine("total working hours: {0}, total worked days: {1}", totalHrs, totalWorkingDays);
+            empWage = Emp_Rate_per_hour * totalHrs;
+            Console.WriteLine("total employee wage is: " + empWage);
+
+        }
+    }
+}
